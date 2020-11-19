b763627f42c1bab4a7c136ed1d05bbde423d4d53
Pablo Santa Cruz <pablo@roshka.com.py>
2020-11-19 12:33:50 -0300

    Added JUNIT dependency + a little test to check Oracle's DataSource configuration parsing.
    Signed-off-by: Pablo Santa Cruz <pablo@roshka.com.py>

---
+++ src/test/java/TestOracleConnection.java
@@ -0,0 +1,32 @@
+import com.roshka.oracledbqueue.config.OracleDataSourceConfig;
+import com.roshka.oracledbqueue.datasource.OracleDataSourceUtil;
+import oracle.jdbc.OraclePreparedStatement;
+import oracle.sql.ROWID;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class TestOracleConnection {
+
+
+    @Test
+    public void testDatasourceConfiguration() throws IOException, SQLException {
+
+        Properties properties = new Properties();
+        properties.load(new FileReader("conf/test.properties"));
+
+        final OracleDataSourceConfig oracleDataSourceConfig = OracleDataSourceConfig.loadFromProperties(properties);
+
+        final DataSource pooledDataSource = OracleDataSourceUtil.createPooledDataSource(oracleDataSourceConfig);
+        final Connection conn = pooledDataSource.getConnection();
+        assert(conn.getMetaData().getDatabaseProductName().equalsIgnoreCase("Oracle"));
+
+    }
+}
